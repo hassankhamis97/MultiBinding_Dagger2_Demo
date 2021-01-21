92e1efffb4792c016f86734730a80d0999d50a64
Hassan Khamis <52527199+hassankhamis97@users.noreply.github.com>
2021-01-21 15:52:18 +0200

    remove constructor

--- app/src/main/java/com/example/multi_binding_dagger2_demo/MainViewModel.kt
+++ app/src/main/java/com/example/multi_binding_dagger2_demo/MainViewModel.kt
@@ -3,9 +3,9 @@ package com.example.multi_binding_dagger2_demo
 import android.util.Log
 import androidx.lifecycle.ViewModel
 
-class MainViewModel constructor(val name: String): ViewModel() {
+class MainViewModel : ViewModel() {
     private val TAG = "MultiBinding_Dagger2_Demo"
     init {
-        Log.d(TAG, "view Model -> name = $name")
+        Log.d(TAG, "view Model -> initialize MainViewModel")
     }
 }
