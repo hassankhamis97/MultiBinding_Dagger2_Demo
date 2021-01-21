6a4fcb6acfc19dc6d7622f0ac8a43fdbfcac00d7
Hassan Khamis <52527199+hassankhamis97@users.noreply.github.com>
2021-01-21 11:02:54 +0200

    use binds instead of provide

--- app/src/main/java/com/example/multi_binding_dagger2_demo/di/AppModule.kt
+++ app/src/main/java/com/example/multi_binding_dagger2_demo/di/AppModule.kt
@@ -1,25 +1,29 @@
 package com.example.multi_binding_dagger2_demo.di
 
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.multibindings.IntoSet
+import javax.inject.Inject
 
 
 interface StringProvider {
     fun provide(): String
 }
 
-class HelloWorldStringProviderImp: StringProvider {
+class HelloWorldStringProviderImp @Inject constructor(): StringProvider {
     override fun provide(): String {
         return "Hello World"
     }
 }
 
 @Module
-class AppModule {
-    @Provides
-    fun provideStringProviderImp(): StringProvider{
-        return HelloWorldStringProviderImp()
-    }
+abstract class AppModule {
+//    @Provides
+//    fun provideStringProviderImp(): StringProvider{
+//        return HelloWorldStringProviderImp()
+//    }
 
+    @Binds
+    abstract fun bindHelloWorld(helloWorldStringProviderImp: HelloWorldStringProviderImp): StringProvider
 }
